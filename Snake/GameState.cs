d1c1829f3df3c503f36379eb89187def99ecde24
Tihara Egodage <tiharaeg@gmail.com>
2024-09-01 13:02:54 +0530

    Add project files.

---
+++ Snake/GameState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class GameState
+    {
+        public int Rows { get; }
+        public int Cols { get; }
+        public GridValue[,] Grid { get; }
+    }
+}
